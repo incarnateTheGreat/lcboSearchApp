194c88b226addd0ffbfddcbd3fb48e609abffdf3
GT <incarnate@rogers.com>
2018-01-24 14:46:33 -0500

    Put port number in Server log statement.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express'),
       app = express(),
       path = require('path'),
-      port = 8080;
+      port = 9090;
 
 //All static resouces are placed in the 'public' directory.
 app.use(express.static("public"));
 
 app.listen(port);
+console.log('Running on :9090...');
